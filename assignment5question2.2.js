8457992c153824d997fe452803d4fb3ca2d8d980
Avery Haskell <124199288+averyhas@users.noreply.github.com>
2023-04-29 18:54:12 -0400

    Update assignment5question2.2.js

--- assignment5question2.2.js
+++ assignment5question2.2.js
@@ -4,57 +4,54 @@
 function setup() {
   createCanvas(960, 540);
   
-  flowers = [];
+  bubbles = [];
 
 // Loop which draws 50 jittering bubbles.
 
-  for (let flowersDrawn = 0; flowersDrawn < 50; flowersDrawn++) {
+  for (let bubblesDrawn = 0; bubblesDrawn < 50; bubblesDrawn++) {
     let thisX = random(width);
     let thisY = random(height);
-    let flowerSize = random(0.1, 0.7);
-    // let flowerColor = fill("hsla( 340, 60%, 65%, random(0.1, 0.5) )")
+    let thisR = random(5, 100)
+    let thisHue = random(360);
     
 // Call the class/constructor & create parameters.
-    flowers[flowersDrawn] = new Flower(thisX, thisY, flowerSize);
+    bubbles[bubblesDrawn] = new Bubble(thisX, thisY, thisR, thisHue);
   }
 }
 
-// Moves & Shows each flower.
+// Moves & Shows each bubble.
 function draw() {
-  background("paleturquoise");
+  background("black");
   
-  for (let flowersShown = 0; flowersShown < flowers.length; flowersShown++) {
+  for (let bubblesShown = 0; bubblesShown < bubbles.length; bubblesShown++) {
     // Call both Show + Move for the sprite(s).
-    flowers[flowersShown].move();
-    flowers[flowersShown].show();
+    bubbles[bubblesShown].move();
+    bubbles[bubblesShown].show();
   }
 }
 
 // Class which holds the "instructions" to create a bubble.
-class Flower {
+class Bubble {
 // Set the parameters according to this class.
-  constructor(x, y, flowerSize) {
+  constructor(x, y, r, hue) {
     this.x = x;
     this.y = y;
-    this.size = flowerSize;
+    this.r = r;
+    this.color = "hsla(" + parseInt(hue) + ", 100%, 90%, 1)";
+
   }
 // Instructions to move the sprite.
   move() {
-    this.x = this.x + random(-3, 3);
-    this.y = this.y + random(-3, 3);
+    this.x = this.x + random(-5, 5);
+    this.y = this.y + random(-5, 5);
   }
 // Instructions to draw the sprite.
   show() {
     push();
-    translate(this.x, this.y)
-    scale(this.size);
-    for (let petalsDrawn = 0; petalsDrawn < 5; petalsDrawn++) 
-    {
-    fill("hsla(340, 60%, 85%, 0.5)");
-    noStroke();
-    rotate(5);
-    ellipse(0, 0, 60, 200);
-    }
+    stroke(this.color);
+    strokeWeight(4);
+    noFill();
+    ellipse(this.x, this.y, this.r * 2);
     pop();
   }
 }
